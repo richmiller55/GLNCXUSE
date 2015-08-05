dcc8019125eca66c1c110bfe872ed307bcad60b3
Rich Miller <rich@richmiller.com>
2015-08-04 19:26:03 -0700

    runs and reads and prints

--- openread.cpp
+++ openread.cpp
@@ -2,6 +2,11 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
+#include <vector>
+
+#include "split.h"
+
 using namespace std;
 
 int main () {
@@ -9,11 +14,18 @@ int main () {
     string _dir  ("/home/co-inet/data/wmgln/");
     string _file  ("08925485US00.as2_califOptical.file.201508022147466A92D40C.in");
     ifstream myfile ( (_dir + _file).c_str());
+
     if (myfile.is_open())
         {
             while ( getline (myfile,line) )
                 {
-                    cout << line << '\n';
+                    std::vector<std::string> x = split(line, '~');
+                    int theSize = x.size();
+                    for (int i = 0; theSize > i; ++i) {
+                        cout << x[i] << '\n';
+                    }
+
+                    // cout << x << '\n';
                 }
             myfile.close();
         }
---
+++ split.h
@@ -0,0 +1,6 @@
+#ifndef SPLIT_H
+#define SPLIT_H
+//  std::vector<std::string> &split(const std::string &s, char delim, <std::string> &elems);
+
+std::vector<std::string> split(const std::string &s, char delim);
+#endif
